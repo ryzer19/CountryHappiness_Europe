946012d3d13322201e012ec3d0255b32cf77d3c0
Ryan Johnston <RyanoJohnston@hotmail.com>
2021-03-02 16:42:32 +0000

    data cleaned & average happiness retrieved

--- Happiness.R
+++ Happiness.R
@@ -8,25 +8,32 @@ countries_income <- read.csv(file = '/Users/ryanjohnston/development/r/dataminin
 year1 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year1.csv')
 year2 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year2.csv')
 
-#removing areas not in europe
+#removing areas not in europe or income dataset 
     countries_income <- countries_income[-c(16,28,30,31,42,44,45), ]
-    year1 <- year1[-c(18,32,34,35,46,48,49),]
-    year2 <- year2[-c(19,29,36,37,45,47,49),]
+    year1 <- year1[-c(48,35,14,49,22,34,29,46,32,12,18),]
+    year2 <- year2[-c(47,36,16,49,21,37,27,45,29,13,19),]
     
-    #removing areas not in income dataset
-    year1 <- year1[-c(14,21,29,12),]
-    year2 <- year2[-c(16,22,27,13),]
+      #changing column names year1 & year2
+    names(year1)[1:10] <- c("Country","Region","Happiness_Rank",
+                            "Happiness_Score","GDP_Per_Capita","Family",
+                            "Life_Expectancy","Freedom","Corruption_Perception","Generosity")
+    names(year2)[1:10] <- c("Country","Region","Happiness_Rank",
+                            "Happiness_Score","GDP_Per_Capita","Family",
+                            "Life_Expectancy","Freedom","Corruption_Perception","Generosity")
     
-    #getting mean values for 5 years -
+    #joins data frame year1 & year2 based off similar 'country' field
+    joined_df <- left_join(year1, year2, 
+                               by = c("Country" = "Country"))
     
-    #adding empty columns
+
+    #gets average values for happiness rating
+    average_happiness <- (joined_df[4] + joined_df[13])/2
+    
+     #adding empty columns
     countries_income[,5] <- NA
     countries_income[,6] <- NA
     countries_income[,7] <- NA
     
-    #changing new column names
-    names(countries_income)[5] <- "Happiness_Score"
-
     
     
     
