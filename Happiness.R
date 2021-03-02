802a25874ba1ed2111dcc6106ec88ebe1975d421
Ryan Johnston <RyanoJohnston@hotmail.com>
2021-03-02 18:50:30 +0000

    typos fixed on excel & row 33-42 added

--- Happiness.R
+++ Happiness.R
@@ -10,7 +10,7 @@ year1 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year1.csv
 year2 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year2.csv')
 
 #removing areas not in europe or income dataset 
-    countries_income <- countries_income[-c(16,28,30,31,42,44,45), ]
+    countries_income <- countries_income[-c(16,28,30,31,42,44,45),]
     year1 <- year1[-c(48,35,14,49,22,34,29,46,32,12,18),]
     year2 <- year2[-c(47,36,16,49,21,37,27,45,29,13,19),]
     
@@ -26,14 +26,20 @@ year2 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year2.csv
     joined_df <- left_join(year1, year2, 
                                by = c("Country" = "Country"))
     
+    joined_df_countries_added <- left_join(year1, countries_income, 
+                           by = c("Country" = "Country"))
+    
+    #adds country names to average
+    joined_df$Country <- paste(joined_df$Country)
+    
+    #adds the average happiness into the column that used to have the happiness for 1 year
+    joined_df$Happiness_Score.x <- paste(average_happiness$Happiness_Score)
+    
+    #changes name for column 1 in avg happiness data frame
+    names(average_happiness)[1] <- "Happiness_Score"
 
     #gets average values for happiness rating
     average_happiness <- (joined_df[4] + joined_df[13])/2
-    
-     #adding empty columns
-    countries_income[,5] <- NA
-    countries_income[,6] <- NA
-    countries_income[,7] <- NA
     
     
     
