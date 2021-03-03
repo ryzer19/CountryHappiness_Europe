5edd8edfdfbe6793364a3ef8c914a8f98415b299
darragh1612 <x17469074@student.ncirl.ie>
2021-03-03 13:50:08 +0000

    Average dataframes done

--- Happiness.R
+++ Happiness.R
@@ -4,13 +4,18 @@ library(data.table)
 
 #test2
 
-#reading datasets in
+#reading datasets in (Ryan)
 countries_income <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/countries.csv')
 year1 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year1.csv')
 year2 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year2.csv')
 
+#reading datasets in (Darragh)
+countries_income <- read.csv(file = 'C:/Users/darra/OneDrive/Documents/datamining/countryhappiness_europe/countries.csv')
+year1 <- read.csv(file = 'C:/Users/darra/OneDrive/Documents/datamining/countryhappiness_europe/year1.csv')
+year2 <- read.csv(file = 'C:/Users/darra/OneDrive/Documents/datamining/countryhappiness_europe/year2.csv')
+
 #removing areas not in europe or income dataset 
-    countries_income <- countries_income[-c(16,28,30,31,42,44,45), ]
+    countries_income <- countries_income[-c(16,28,30,31,42,44,45),]
     year1 <- year1[-c(48,35,14,49,22,34,29,46,32,12,18),]
     year2 <- year2[-c(47,36,16,49,21,37,27,45,29,13,19),]
     
@@ -26,16 +31,23 @@ year2 <- read.csv(file = '/Users/ryanjohnston/development/r/datamining/year2.csv
     joined_df <- left_join(year1, year2, 
                                by = c("Country" = "Country"))
     
+    joined_df_countries_added <- left_join(year1, countries_income, 
+                           by = c("Country" = "Country"))
+    
+    #adds country names to average
+    joined_df$Country <- paste(joined_df$Country)
 
     #gets average values for happiness rating
     average_happiness <- (joined_df[4] + joined_df[13])/2
+    average_gdp <- (joined_df[5] + joined_df[14])/2
+    average_family <- (joined_df[6] + joined_df[15])/2
+    average_lifeexpectancy <- (joined_df[7] + joined_df[16])/2
+    average_freedom <- (joined_df[8] + joined_df[17])/2
+    average_corruption <- (joined_df[9] + joined_df[18])/2
+    average_generosity <- (joined_df[10] + joined_df[19])/2
     
-     #adding empty columns
-    countries_income[,5] <- NA
-    countries_income[,6] <- NA
-    countries_income[,7] <- NA
+    #changes name for column 1 in avg happiness data frame
+    names(average_happiness)[1] <- "Happiness_Score"
     
-    
-    
-    
-    
+    #adds the average happiness into the column that used to have the happiness for 1 year
+    joined_df$Happiness_Score.x <- paste(average_happiness$Happiness_Score)
